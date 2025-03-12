f85375aa4f3e60071f16efee361810ecb2fbe5ce
Vodka_jpg <pzajchowski04@gmail.com>
2025-03-12 09:32:56 +0100

    Inicjalizacja pluginu

---
+++ src/main/java/org/VodkaJpg/multitool/Multitool.java
@@ -0,0 +1,17 @@
+package org.VodkaJpg.multitool;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Multitool extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
